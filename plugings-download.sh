19b33d2585e8219a43c4165491cd27ba9be91fed
Marty42780 <59181926+Marty42780@users.noreply.github.com>
2023-02-09 20:54:05 +0000

    Added you-should-use in plugins-download.sh

--- plugings-download.sh
+++ plugings-download.sh
@@ -6,6 +6,7 @@
 # Oh-My-Zsh Plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/MichaelAquilina/zsh-you-should-use.git $ZSH_CUSTOM/plugins/you-should-use
 
 # Vim Plugins 
 
